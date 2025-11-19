36331f9f5b525e3b8f0592a728ff64d924523690
cauatn <tavarescauac@gmail.com>
2025-11-19 10:58:14 -0300

    feat: middleware de autenticação com proteção de rotas

---
+++ apps/web/src/features/auth/middleware/auth-middleware.ts
@@ -0,0 +1,48 @@
+import { redirect } from "@tanstack/react-router";
+import { authService } from "../services/auth.service";
+
+const PUBLIC_ROUTES = [
+  {
+    path: "/auth/login",
+    whenAuthenticated: "redirect",
+  },
+] as const;
+
+const REDIRECT_WHEN_NOT_AUTHENTICATED_ROUTE = "/auth/login";
+const REDIRECT_WHEN_AUTHENTICATED_ROUTE = "/app/owner";
+
+export function authMiddleware(context: { location: { pathname: string } }) {
+  const pathname = context.location.pathname;
+  const publicRoute = PUBLIC_ROUTES.find((route) => route.path === pathname);
+  const isPublicRoute = !!publicRoute;
+  const isAuthenticated = authService.isAuthenticated();
+
+  if (!isAuthenticated && isPublicRoute) {
+    console.log("not authenticated and public route");
+    return;
+  }
+
+  if (!isAuthenticated && !isPublicRoute) {
+    console.log("redirecting to not authenticated route");
+    throw redirect({
+      to: REDIRECT_WHEN_NOT_AUTHENTICATED_ROUTE,
+    });
+  }
+
+  if (
+    isAuthenticated &&
+    publicRoute &&
+    publicRoute.whenAuthenticated === "redirect"
+  ) {
+    console.log("redirecting to authenticated route");
+    throw redirect({
+      to: REDIRECT_WHEN_AUTHENTICATED_ROUTE as any,
+    });
+  }
+
+  if (isAuthenticated && !isPublicRoute) {
+    return;
+  }
+
+  return;
+}
