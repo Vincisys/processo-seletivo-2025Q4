e6c34a7247b90fa659c0169eb7afc9d888d3c038
cauatn <tavarescauac@gmail.com>
2025-11-20 15:35:58 -0300

    refactor: remove página de API Status e redireciona rota raiz para login

--- apps/web/src/routes.tsx
+++ apps/web/src/routes.tsx
@@ -2,7 +2,6 @@ import { createBrowserRouter, Navigate } from "react-router-dom";
 import { RootLayout } from "./routes/root-layout";
 import { AppLayout } from "./routes/app-layout";
 import { LoginPage } from "./features/auth/pages/login";
-import { HomePage } from "./features/home/pages/home";
 import { AssetsPage } from "./features/app/assets/pages/assets";
 import { OwnerPage } from "./features/app/owners/pages/owner";
 import {
@@ -17,7 +16,7 @@ export const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <HomePage />,
+        element: <Navigate to="/auth/login" replace />,
       },
       {
         path: "auth/login",
