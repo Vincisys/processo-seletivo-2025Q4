8617068825f4b6220424fab18e992e504ef2f075
cauatn <tavarescauac@gmail.com>
2025-11-19 21:03:14 -0300

    feat: atualiza página de assets com integração completa

--- apps/web/src/features/app/assets/pages/assets.tsx
+++ apps/web/src/features/app/assets/pages/assets.tsx
@@ -1,14 +1,40 @@
-import { Card } from "@/components/ui/card";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { AssetDataTable } from "../components/Table/data-table";
+import { useQuery } from "@tanstack/react-query";
+import { getAllAssets } from "../services/asset";
 
 export function AssetsPage() {
+  const { data: assets, isLoading } = useQuery({
+    queryKey: ["assets"],
+    queryFn: () => getAllAssets(),
+  });
+
   return (
-    <div className="flex min-h-[calc(100vh-4rem)] items-center justify-center p-4">
-      <Card className="w-full max-w-md p-6">
-        <div className="mb-6 space-y-2 text-center">
-          <h1 className="text-2xl font-bold">Ativos</h1>
-          <p className="text-muted-foreground">Gerencie os ativos do sistema</p>
-        </div>
+    <div className="flex flex-col gap-4 p-4">
+      <div className="mb-6 space-y-2">
+        <h1 className="text-2xl font-bold">Ativos</h1>
+        <p className="text-muted-foreground">Gerencie os ativos do sistema</p>
+      </div>
+      <Card>
+        <CardContent className="flex flex-row">
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/7486/7486747.png"
+            width={150}
+            height={150}
+          />
+          <div className="flex flex-col gap-2 px-4">
+            <CardHeader className="text-2xl font-bold p-0">
+              Titulo do Card
+            </CardHeader>
+            <span>
+              Existe uma teoria que diz que, se um dia alguém descobrir
+              exatamente para que serve o Universo e por que ele está aqui, ele
+              desaparecerá instantaneamente.
+            </span>
+          </div>
+        </CardContent>
       </Card>
+      <AssetDataTable assets={assets} isLoading={isLoading || false} />
     </div>
   );
 }
